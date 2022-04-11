861d83b48f6be6f576c7e3a70c2c685ec8e577be
AreyBRSK <areybrsk@gmail.com>
2022-04-11 13:35:46 +0300

    Mock data added

--- PensonList_Lesson2.7/Service/DataManager.swift
+++
@@ -1,7 +0,0 @@
-//
-//  DataManager.swift
-//  PensonList_Lesson2.7
-//
-//  Created by Андрей Барсук on 11.04.2022.
-//
-
---
+++ PensonList_Lesson2.7/Service/DataStorage.swift
@@ -0,0 +1,69 @@
+//
+//  DataStorage.swift
+//  PensonList_Lesson2.7
+//
+//  Created by Андрей Барсук on 11.04.2022.
+//
+
+class DataStorage {
+  let firstNames = [
+    "Andrey",
+    "Brian",
+    "Clementine",
+    "Dolores",
+    "Ethan",
+    "Fyodor",
+    "Gregory",
+    "Jason",
+    "Kim",
+    "Liam",
+    "Michael",
+    "Natalie",
+    "Otto"
+  ]
+  let lastNames = [
+    "Abernathy",
+    "Blossom",
+    "Cambridge",
+    "Dalmatian",
+    "Ether",
+    "Ford",
+    "Greyjoy",
+    "Jackson",
+    "Kimberly",
+    "Letterman",
+    "Mason",
+    "Newport",
+    "Openhaimer"
+  ]
+  let phoneNumbers = [
+    "+79955345235",
+    "+79625626457",
+    "+71534753632",
+    "+79068545263",
+    "+77563742683",
+    "+79153453426",
+    "+79973565262",
+    "+77568734226",
+    "+77567346236",
+    "+78623467226",
+    "+79956756876",
+    "+79964572632",
+    "+79675872463"
+  ]
+  let emailAddresses = [
+    "9955345235@gmail.com",
+    "9625626457@gmail.com",
+    "1534753632@gmail.com",
+    "9068545263@gmail.com",
+    "7563742683@gmail.com",
+    "9153453426@gmail.com",
+    "9973565262@gmail.com",
+    "7568734226@gmail.com",
+    "7567346236@gmail.com",
+    "8623467226@gmail.com",
+    "9956756876@gmail.com",
+    "9964572632@gmail.com",
+    "9675872463@gmail.com"
+  ]
+}
