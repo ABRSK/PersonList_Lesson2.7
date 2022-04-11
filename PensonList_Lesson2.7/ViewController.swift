6765339fb8d012db6dd7953110bd7595d6a340c9
AreyBRSK <areybrsk@gmail.com>
2022-04-11 11:39:14 +0300

    Initial Commit

---
+++ PensonList_Lesson2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PensonList_Lesson2.7
+//
+//  Created by Андрей Барсук on 11.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
