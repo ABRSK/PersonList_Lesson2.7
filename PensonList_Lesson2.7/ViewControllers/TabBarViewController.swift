b39150237ee64d6003d812cc5d08aa6334124b0c
AreyBRSK <areybrsk@gmail.com>
2022-04-11 13:14:37 +0300

    Added ViewController files

---
+++ PensonList_Lesson2.7/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  PensonList_Lesson2.7
+//
+//  Created by Андрей Барсук on 11.04.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
