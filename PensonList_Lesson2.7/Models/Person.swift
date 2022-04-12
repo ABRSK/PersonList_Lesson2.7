d5e8746fe25c21bffb941e11e818b14d7b34d1e0
AreyBRSK <areybrsk@gmail.com>
2022-04-12 12:30:20 +0300

    Added static method to populate model

--- PensonList_Lesson2.7/Models/Person.swift
+++ PensonList_Lesson2.7/Models/Person.swift
@@ -11,7 +11,30 @@ struct Person {
   let phoneNumber: String
   let emailAddress: String
   
-  var fullName: String {
+  var getFullName: String { // Getter для получения строки
     "\(firstName) \(lastName)"
+  }
+  
+  static func getPersons() -> [Person] {
+    var list: [Person] = []
+    
+    let firstNames = DataStorage.data.firstNames.shuffled()
+    let lastNames = DataStorage.data.lastNames.shuffled()
+    let phoneNumbers = DataStorage.data.phoneNumbers.shuffled()
+    let emailAddresses = DataStorage.data.emailAddresses.shuffled()
+    // .shuffled() Returns the elements of the sequence, shuffled.
+    
+    for index in 0..<firstNames.count {
+      let person = Person(
+        firstName: firstNames[index],
+        lastName: lastNames[index],
+        phoneNumber: phoneNumbers[index],
+        emailAddress: emailAddresses[index]
+      )
+      
+      list.append(person)
+    }
+    
+    return list
   }
 }
